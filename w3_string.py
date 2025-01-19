64a19031bc2a29c073720643e9027ff3ba677875
adiyaturkpen <a.turkpen@gmail.com>
2025-01-19 22:22:37 +0500

    addex

---
+++ w3_string.py
@@ -0,0 +1,164 @@
+#1
+'''print("Hello, World!")'''
+#2
+'''import sys
+print(sys.version)'''
+#3
+'''if 1 > 0:
+ print("1 is greater than 0!") '''
+#4
+'''asdfghjkl;'''
+#5
+'''x = 1
+y = "abc"
+print(x)
+print(y)'''
+#6
+'''x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)'''
+#7
+'''x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0'''
+#8
+'''x = 1
+y = "abc"
+print(type(x))
+print(type(y))'''
+#9
+'''a = 4
+A = "Sally"
+#A will not overwrite a'''
+#10
+'''myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"'''
+#11
+'''x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)'''
+#12
+'''x = y = z = "Orange"
+print(x)
+print(y)
+print(z)'''
+#13
+'''fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)'''
+#14
+'''x = "Python is awesome"
+print(x)'''
+#15
+'''x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)'''
+#16
+'''x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)'''
+#17
+'''x = 5
+y = 10
+print(x + y)'''
+#18
+'''x = 5
+y = "John"
+print(x, y)'''
+#19
+'''x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()'''
+#20
+'''x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)'''
+#21
+'''x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)'''
+#22
+'''x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))'''
+#23
+'''x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+#24
+'''x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+#25
+'''x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+#26
+'''x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))'''
+#27
+'''x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))'''
+#28
